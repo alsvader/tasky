5de7da17c8870018cc5cab1468322431787b4694
Aarón López Sosa <aaron.sosa@unosquare.com>
2025-12-19 00:19:39 -0600

    Add floating menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from "../hooks/useTheme";
 import TaskyIcon from "../assets/tasky-icon.webp";
 import TaskyIconDark from "../assets/tasky-icon-dark.webp";
+import { Menu } from "./Menu";
 
 export function Header() {
   const { theme, toggleTheme } = useTheme();
@@ -25,54 +26,7 @@ export function Header() {
             </span>
           </button>
         </div>
-        {/* <button className="flex cursor-pointer items-center justify-center overflow-hidden rounded-full size-11 bg-gray-200 dark:bg-card-dark text-text-secondary-light dark:text-text-secondary-dark border border-border-light dark:border-border-dark">
-          <span className="material-symbols-outlined text-xl">settings</span>
-        </button> */}
-        {/* <div
-          className="rounded-full size-11 flex items-center justify-center bg-gray-200 dark:bg-card-dark relative"
-          data-alt="User profile picture, abstract placeholder"
-        >
-          <span className="material-symbols-outlined">account_box</span>
-        </div> */}
-        <details className="relative group">
-          <summary
-            aria-haspopup="menu"
-            className="list-none [&::-webkit-details-marker]:hidden cursor-pointer outline-none"
-          >
-            <div
-              className="rounded-full size-11 flex items-center justify-center bg-gray-200 dark:bg-card-dark relative"
-              data-alt="User profile picture, abstract placeholder"
-            >
-              <span className="material-symbols-outlined">account_box</span>
-            </div>
-          </summary>
-          <div
-            className="absolute right-0 top-full mt-2 w-56 origin-top-right rounded-xl bg-card-light dark:bg-card-dark border border-border-light dark:border-border-dark shadow-xl focus:outline-none z-50 overflow-hidden"
-            role="menu"
-          >
-            <div className="p-1">
-              <button
-                className="flex items-center gap-3 px-3 py-2.5 text-sm font-medium text-text-light dark:text-text-dark rounded-lg hover:bg-background-light dark:hover:bg-background-dark transition-colors"
-                role="menuitem"
-              >
-                <span className="material-symbols-outlined text-[20px] text-text-secondary-light dark:text-text-secondary-dark">
-                  settings
-                </span>
-                Settings
-              </button>
-              <div className="h-px bg-border-light dark:bg-border-dark my-1 mx-2"></div>
-              <button
-                className="flex items-center gap-3 px-3 py-2.5 text-sm font-medium text-red-500 rounded-lg hover:bg-red-50 dark:hover:bg-red-900/10 transition-colors"
-                role="menuitem"
-              >
-                <span className="material-symbols-outlined text-[20px]">
-                  logout
-                </span>
-                Logout
-              </button>
-            </div>
-          </div>
-        </details>
+        <Menu />
       </div>
     </header>
   );
---
+++ src/components/Menu.tsx
@@ -0,0 +1,81 @@
+import { useState, useRef, useEffect } from "react";
+import { cn } from "../utils/cn";
+
+export function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef]);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const onSettings = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.currentTarget.blur();
+    toggleMenu();
+  };
+
+  return (
+    <div className="relative group" ref={menuRef}>
+      <div
+        aria-haspopup="menu"
+        className="list-none [&::-webkit-details-marker]:hidden cursor-pointer outline-none"
+        onClick={toggleMenu}
+        role="button"
+      >
+        <div
+          className="rounded-full size-11 flex items-center justify-center bg-gray-200 dark:bg-card-dark relative"
+          data-alt="User profile picture, abstract placeholder"
+        >
+          <span className="material-symbols-outlined">account_box</span>
+        </div>
+      </div>
+      <div
+        className={cn(
+          'hidden absolute right-0 top-full mt-2 w-56 origin-top-right rounded-xl bg-card-light dark:bg-card-dark border border-border-light dark:border-border-dark shadow-xl focus:outline-none z-50 overflow-hidden"',
+          {
+            block: isOpen,
+          }
+        )}
+        role="menu"
+      >
+        <div className="p-1">
+          <button
+            className={`w-full flex items-center gap-3 px-3 py-2.5 text-sm font-medium text-text-light dark:text-text-dark rounded-lg transition-colors hover:cursor-pointer hover:bg-background-light dark:hover:bg-background-dark`}
+            role="menuitem"
+            onClick={onSettings}
+          >
+            <span className="material-symbols-outlined text-[20px] text-text-secondary-light dark:text-text-secondary-dark">
+              settings
+            </span>
+            Settings
+          </button>
+          <div className="h-px bg-border-light dark:bg-border-dark my-1 mx-2" />
+          <button
+            className="w-full flex items-center gap-3 px-3 py-2.5 text-sm font-medium text-red-500 rounded-lg hover:bg-background-light dark:hover:bg-background-dark transition-colors hover:cursor-pointer"
+            role="menuitem"
+          >
+            <span className="material-symbols-outlined text-[20px]">
+              logout
+            </span>
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
